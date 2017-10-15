157bad299c0c64603ceee56c54c8ebd80d388e2b
user <user@debian-test1.home>
2017-10-15 12:25:54 -0400

    e

--- tasks.py
+++ tasks.py
@@ -2,11 +2,12 @@ from celery_fun.my_worker import app
 import time
 
 @app.task
-def gitclone(giturl):
+def gitclone(self, giturl):
 	print('sleeping')
+	self.update_state(state='sleeping')
 	time.sleep(5)
 	print('cloning %s' % giturl)
 
 @app.task
-def run_command(cmd):
+def run_command(self, cmd):
 	print('running %s' %cmd)
